9b94ef8b882e8aa6666fac248ee56757ae3de93c
Elitebook <antolovic.stjepan@gmail.com>
2025-08-06 22:41:15 +0200

    update

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,5 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:5001/api/',
-    stripePublicKey: 'pk_test_51Rs6sfRFCx9RQ7Rjz6UrHCbljgFqBK6xjIbS5jctFtx5W43Sna4K4NxCYe7bcxeDpcbCoLitETfId1POsv4sQxjT00a3DEXh4L'
+    apiUrl: 'https://localhost:5001/api/'
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     apiUrl: 'api/',
-    stripePublicKey: 'pk_test_51Rs6sfRFCx9RQ7Rjz6UrHCbljgFqBK6xjIbS5jctFtx5W43Sna4K4NxCYe7bcxeDpcbCoLitETfId1POsv4sQxjT00a3DEXh4L'
+    stripePublicKey: '12345'
 };
